b305e643091898eaf50d9470b334ccaf3c671188
Domange <backinsmail@gmail.com>
2020-01-29 15:35:18 +0100

    Add default content when no votes

--- src/views/Votes.vue
+++ src/views/Votes.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container v-if="votes.length">
       <v-card class="mx-auto" max-width="700" style="margin-top: -64px;">
       <v-toolbar flat>
         <v-toolbar-title class="black--text" align-center>Votes</v-toolbar-title>
@@ -23,6 +23,24 @@
               <v-spacer></v-spacer>
               <v-btn text color='indigo' dark>Suite</v-btn>
             </v-card-actions>
+          </v-card>   
+          <v-divider light></v-divider>
+    </v-card>
+  </v-container>
+  <v-container v-else>
+      <v-card class="mx-auto" max-width="700" style="margin-top: -64px;">
+      <v-toolbar flat>
+        <v-btn rounded class="btn btn-primary">
+          <router-link to="/add-vote">Créer un vote</router-link>
+        </v-btn>
+      </v-toolbar>
+      <v-divider></v-divider>
+          <v-card class="elevation-12" style="margin-top: 20px;">
+                <v-toolbar dark color="blue">
+                  <v-toolbar-title>
+                    Il est nécessaire de créer un vote pour pouvoir continuer
+                  </v-toolbar-title>  
+                </v-toolbar>
           </v-card>   
           <v-divider light></v-divider>
     </v-card>
